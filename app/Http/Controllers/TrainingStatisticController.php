4e840a2524cae72935194881222cc69c82142a00
armenium <armen@digidez.com>
2022-10-03 03:24:57 +0200

    7.14.5 Smishing menu should look like scenario’s menu (2)

--- app/Http/Controllers/TrainingStatisticController.php
+++ app/Http/Controllers/TrainingStatisticController.php
@@ -9,66 +9,74 @@ use App\Models\TrainingStatistic;
 use App\Repositories\TrainingStatisticRepository;
 use Yajra\Datatables\Datatables;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
-class TrainingStatisticController extends AppBaseController
-{
-    private $trainingStatisticRepository;
+class TrainingStatisticController extends AppBaseController{
+	private $trainingStatisticRepository;
 
-    public function __construct(TrainingStatisticRepository $trainingStatisticRepo)
-    {
-        parent::__construct();
+	public function __construct(TrainingStatisticRepository $trainingStatisticRepo){
+		parent::__construct();
 
-        $this->trainingStatisticRepository = $trainingStatisticRepo;
-    }
+		$this->trainingStatisticRepository = $trainingStatisticRepo;
+	}
 
-    public function index(Request $request)
-    {
-        return view('training_statistics.index');
-    }
+	public function index(Request $request){
+		$years = DB::table('training_statistics')
+			->whereNotNull('start_training')
+			->groupBy('value')
+			->orderBy('value')
+			->get([DB::raw('EXTRACT(YEAR FROM start_training) AS value')])
+			->toArray();
 
-    public function table(Request $request)
-    {
-        if ($request->ajax()) {
+		return view('training_statistics.index', ['years' =>$years]);
+	}
 
-            $this->trainingStatisticRepository->pushCriteria(BelongsToCompanyCriteria::class);
-            $trainingStatistic = $this->trainingStatisticRepository->all();
+	public function table(Request $request){
+		if($request->ajax()){
 
-            return Datatables::of($trainingStatistic)
+			$this->trainingStatisticRepository->pushCriteria(new BelongsToCompanyCriteria(true));
+			$trainingStatistic = $this->trainingStatisticRepository->all();
+			$trainingStatistic->where(DB::raw('EXTRACT(YEAR FROM start_training)'), $request->post('columns')[3]['search']['value']);
+			#dd($request->post('columns')[3]['search']['value']);
 
-                ->addIndexColumn()
-                ->addColumn('recipient', function($row){
-                    return $row->recipient ? $row->recipient->first_name . ' ' . $row->recipient->last_name : '';
-                })
+			return Datatables::of($trainingStatistic)
+				->setRowAttr([
+					'data-year' => function($row){
+						$c = new Carbon($row->start_training);
+						$year = $c->year;
+						return $year;
+					}
+				])
+				->addIndexColumn()
+				->addColumn('recipient', function($row){
+					return $row->recipient ? $row->recipient->first_name.' '.$row->recipient->last_name : '';
+				})
+				->addIndexColumn()
+				->addColumn('recipient_email', function($row){
+					return $row->recipient ? $row->recipient->email : '';
+				})
+				->addIndexColumn()
+				->addColumn('company', function($row){
+					return $row->company ? $row->company->name : '';
+				})
+				->addIndexColumn()
+				->addColumn('time', function($row){
 
-                ->addIndexColumn()
-                ->addColumn('recipient_email', function($row){
-                    return $row->recipient ? $row->recipient->email: '';
-                })
+					$diff = '';
 
-                ->addIndexColumn()
-                ->addColumn('company', function($row){
-                    return $row->company ? $row->company->name : '';
-                })
+					if($row->start_training && $row->finish_training){
+						$from = new Carbon($row->start_training);
+						$till = new Carbon($row->finish_training);
+						$diff = $till->diffInMinutes($from).' min';
+					}
 
+					return $diff;
 
-                ->addIndexColumn()
-                ->addColumn('time', function($row){
+				})
+				->make(true);
+		}
 
-                    $diff = '';
+		return false;
+	}
 
-                    if ($row->start_training && $row->finish_training) {
-                        $from = new Carbon($row->start_training);
-                        $till = new Carbon($row->finish_training);
-                        $diff = $till->diffInMinutes($from) . ' min';
-                    }
-
-                    return $diff;
-
-                })
-
-                ->make(true);
-        }
-
-        return false;
-    }
 }
--- resources/views/training_statistics/table.blade.php
+++ resources/views/training_statistics/table.blade.php
@@ -1,6 +1,14 @@
-<div>
+<div class="custom-table-data">
     <table class="table data-table table-responsive">
         <thead>
+            <tr class="filters">
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th width="170"></th>
+            </tr>
             <tr>
                 <th>Recipient</th>
                 <th>Email</th>
@@ -10,8 +18,13 @@
                 <th>Time spent</th>
             </tr>
         </thead>
-        <tbody>
-        </tbody>
+        <tbody></tbody>
+        <tfoot>
+            <tr>
+                <th colspan="5" style="text-align:right">Total:</th>
+                <th></th>
+            </tr>
+        </tfoot>
     </table>
 </div>
 
@@ -19,8 +32,11 @@
     $(function(){
 
         var table = $('.data-table').DataTable({
+			//dom: '<"top"iflp<"clear">>rt',
+			dom: '<"toolbar"fl>rt<"bottom"ip>',
             processing: true,
             serverSide: true,
+			pageLength: 50,
             ajax: "{{ url('trainingStatistic/getTable') }}",
             columns: [
                 {data: 'recipient', name: 'recipient'},
@@ -29,8 +45,69 @@
                 {data: 'start_training', name: 'start_training'},
                 {data: 'finish_training', name: 'finish_training'},
                 {data: 'time', name: 'time'},
-            ]
+            ],
+			initComplete: function(){
+				let div = $('<div id="js_year_filter" class="dataTables_year"><label>Filter by years:</label> </div>');
+				let select = $('<select><option value="">All</option></select>');
+
+				$('div.toolbar').prepend(div);
+
+				this.api().columns([3]).every(function(index){
+					var column = this;
+
+					select
+						.appendTo($('div.toolbar #js_year_filter'))
+						.on('change', function(){
+							var val = $.fn.dataTable.util.escapeRegex($(this).val());
+							console.log(val);
+							column
+								.search(val ? val : '', true, false)
+								.draw();
+						});
+
+					@foreach($years as $year)
+					select.append('<option value="{!! $year->value !!}">{!! $year->value !!}</option>');
+                    @endforeach
+				});
+			},
+			"fnDrawCallback": function(oSettings){
+				//updateLangsFlags();
+			},
+			footerCallback: function (row, data, start, end, display) {
+				var api = this.api();
+
+				// Remove the formatting to get integer data for summation
+				var intVal = function(i){
+					return typeof i === 'string' ? i.replace(/ min/g, '') * 1 : typeof i === 'number' ? i : 0;
+				};
+
+				// Total over all pages
+				let total = api.column(5).data().reduce(function(a, b){
+                    return intVal(a) + intVal(b);
+                }, 0);
+
+				// Total over this page
+				let pageTotal = api.column(5, {page: 'current'}).data().reduce(function(a, b){
+                    return intVal(a) + intVal(b);
+                }, 0);
+
+				// Update footer
+				$(api.column(5).footer()).html(pageTotal + ' mins');
+			},
         });
 
-    });
+		//$('div.toolbar').append('<b>Custom tool bar! Text/images etc.</b>');
+
+		var updateLangsFlags = function(){
+			table.rows().every(function(index){
+				//console.log(index);
+				var year = this.data().DT_RowAttr['data-year'];
+				if(year != ''){
+					var $td = $('.dataTable tbody tr:eq(' + index + ') td:eq(3)');
+					$td.html(year);
+				}
+			});
+		};
+
+	});
 </script>
