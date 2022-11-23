5c6f3723951a34e8c4f0a0cade43f75dd612502e
Artur Asatryan <artura@eocenterprise.com>
2022-11-23 16:53:32 +0400

    Move training export tab

--- resources/views/trainings/tabs.blade.php
+++ resources/views/trainings/tabs.blade.php
@@ -1,5 +1,6 @@
 <ul class="nav nav-tabs">
 	<li class="{{ $active == 'trainings' ? 'active' : '' }}"><a href="{!! route('trainings.index') !!}"><i class="fa fa-graduation-cap"></i> Trainings</a></li>
-    <li class="{{ $active == 'training_statistics' ? 'active' : '' }}"><a href="{!! route('trainingStatistics.index') !!}"><i class="fa fa-graduation-cap"></i> Training Statistics</a></li>
-    <li class="{{ $active == 'training_notify_templates' ? 'active' : '' }}"><a href="{!! route('trainingNotifyTemplates.index') !!}"><i class="fa fa-graduation-cap"></i> Training Notify Templates</a></li>
+    <li class="{{ $active == 'training_statistics' ? 'active' : '' }}"><a href="{!! route('trainingStatistics.index') !!}"><i class="fa fa-dashboard"></i> Training Statistics</a></li>
+    <li class="{{ $active == 'training_statistics_export' ? 'active' : '' }}"><a href="{!! route('trainingStatistic.export') !!}"><i class="fa fa-bar-chart"></i> Training Statistics Data Export</a></li>
+    <li class="{{ $active == 'training_notify_templates' ? 'active' : '' }}"><a href="{!! route('trainingNotifyTemplates.index') !!}"><i class="fa fa-envelope"></i> Training Notify Templates</a></li>
 </ul>
